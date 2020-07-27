5e5aa3e26448a3f0a1e91c8b28da4cb527a0eb4b
zgc111 <1461716824@qq.com>
2020-07-27 22:16:41 +0800

    实现接口

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/WeatherInfoController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/WeatherInfoController.java
@@ -1,19 +1,22 @@
 package com.ruoyi.web.controller.tool;
 
-import com.alibaba.fastjson.JSONObject;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 天气信息
  */
 @Controller
 @RequestMapping("/tool/weather")
+@ResponseBody
 public class WeatherInfoController {
 
     @Autowired
@@ -25,17 +28,29 @@ public class WeatherInfoController {
     /**
      * 行政区域接口
      */
-    private String districtURL = " https://restapi.amap.com/v3/config/district?key=7a9247c712ee77b73fa81ec9bd9de896";
+    private String districtURL = "https://restapi.amap.com/v3/config/district?key=7a9247c712ee77b73fa81ec9bd9de896";
 
     /**
      * ip接口
      */
     private String ipURL = "https://restapi.amap.com/v3/ip?key=7a9247c712ee77b73fa81ec9bd9de896";
 
+    /**
+     * 根据城市名称查询天气
+     *
+     * @param keywords
+     * @return
+     */
     @RequestMapping("info")
     @RequiresPermissions("tool:weather:info")
-    public JSONObject info() {
-        restTemplate.getForObject(districtURL,null,JSONObject.class);
-        return null;
+    public JsonOb info(String keywords) {
+        String url = districtURL + "&keywords=" + "合肥";
+        JSONObject restTemplateForObject = restTemplate.getForObject(url, JSONObject.class, JSONObject.class);
+        JSONArray districts =  JSONArray.   restTemplateForObject.get("districts");//districts得到是json数组
+        JSONObject result = districts.getJSONObject(0);
+        String adcode = result.get("adcode").toString();//得到城市编码
+        String wUrl = weatherURL + "&city=" + adcode;
+        JSONObject weatherObject = restTemplate.getForObject(wUrl, JSONObject.class, JSONObject.class);
+        return weatherObject;
     }
 }
