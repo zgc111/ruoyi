de8049c1dfd6050f00b8da8d18ed6f09178ba5a8
zgc <1461716824@qq.com>
2020-07-27 16:58:50 +0800

    添加3个接口数据

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/WeatherInfoController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/WeatherInfoController.java
@@ -1,7 +1,13 @@
 package com.ruoyi.web.controller.tool;
 
+import com.alibaba.fastjson.JSONObject;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * 天气信息
@@ -9,19 +15,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/tool/weather")
 public class WeatherInfoController {
+
+    @Autowired
+    private RestTemplate restTemplate;
     /**
      * 天气接口
      */
-    private String weatherURL="https://restapi.amap.com/v3/weather/weatherInfo?key=7a9247c712ee77b73fa81ec9bd9de896";
+    private String weatherURL = "https://restapi.amap.com/v3/weather/weatherInfo?key=7a9247c712ee77b73fa81ec9bd9de896";
     /**
      * 行政区域接口
      */
-    private String districtURL=" https://restapi.amap.com/v3/config/district?key=7a9247c712ee77b73fa81ec9bd9de896";
+    private String districtURL = " https://restapi.amap.com/v3/config/district?key=7a9247c712ee77b73fa81ec9bd9de896";
 
     /**
      * ip接口
      */
-    private String ipURL="https://restapi.amap.com/v3/ip?key=7a9247c712ee77b73fa81ec9bd9de896";
+    private String ipURL = "https://restapi.amap.com/v3/ip?key=7a9247c712ee77b73fa81ec9bd9de896";
 
-
+    @RequestMapping("info")
+    @RequiresPermissions("tool:weather:info")
+    public JSONObject info() {
+        restTemplate.getForObject(districtURL,null,JSONObject.class);
+        return null;
+    }
 }
