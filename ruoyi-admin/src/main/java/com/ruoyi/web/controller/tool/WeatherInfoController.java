31b6ae4029acf12c8a5897b68829c15a7b1a135b
zgc <1461716824@qq.com>
2020-08-07 13:08:31 +0800

    添加swagger

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/WeatherInfoController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/WeatherInfoController.java
@@ -1,22 +1,19 @@
 package com.ruoyi.web.controller.tool;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * 天气信息
  */
 @Controller
 @RequestMapping("/tool/weather")
-@ResponseBody
 public class WeatherInfoController {
 
     @Autowired
@@ -35,22 +32,10 @@ public class WeatherInfoController {
      */
     private String ipURL = "https://restapi.amap.com/v3/ip?key=7a9247c712ee77b73fa81ec9bd9de896";
 
-    /**
-     * 根据城市名称查询天气
-     *
-     * @param keywords
-     * @return
-     */
     @RequestMapping("info")
     @RequiresPermissions("tool:weather:info")
-    public JsonOb info(String keywords) {
-        String url = districtURL + "&keywords=" + "合肥";
-        JSONObject restTemplateForObject = restTemplate.getForObject(url, JSONObject.class, JSONObject.class);
-        JSONArray districts =  JSONArray.   restTemplateForObject.get("districts");//districts得到是json数组
-        JSONObject result = districts.getJSONObject(0);
-        String adcode = result.get("adcode").toString();//得到城市编码
-        String wUrl = weatherURL + "&city=" + adcode;
-        JSONObject weatherObject = restTemplate.getForObject(wUrl, JSONObject.class, JSONObject.class);
-        return weatherObject;
+    public JSONObject info() {
+        restTemplate.getForObject(districtURL,null,JSONObject.class);
+        return null;
     }
 }
